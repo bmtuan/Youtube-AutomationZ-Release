bccfe3f07e24de9216213fc963a54c951db39537
bmtuan <phoneixhp123@gmail.com>
2025-09-09 17:06:19 +0700

    feat: update file update

--- update.bat
+++ update.bat
@@ -1,15 +1,27 @@
 @ECHO OFF
 ECHO ===========================
-ECHO EXECUTE UPDATE PROGRAM...
+ECHO EXECUTE FORCE PULL UPDATE
 ECHO ===========================
 
-REM Update the repository
+REM Navigate to script directory
+CD /D "%~dp0"
 
+REM Set up Git to store credentials
 git config credential.helper store
+
+REM Clean up and stash any changes
 git gc
-git stash save "Stash"
-git checkout "main"
-git pull --no-rebase https://gitlab.com/bmtuan/youtube-automation-z-application.git
+git reset --hard
+git clean -fd
+git stash save "Backup before force pull"
+
+REM Force switch to main branch and reset to origin
+git checkout main
+git fetch origin
+git reset --hard origin/main
+
+REM Pull latest changes
+git pull --no-rebase
 
 REM Activate the virtual environment
 SET VENV_PATH=%~dp0venv
@@ -21,11 +33,11 @@ IF EXIST "%VENV_PATH%" (
     EXIT /B 1
 )
 
-REM Install the requirements
+REM Install requirements
 pip install -U -r requirements.txt
 
 ECHO ===========================
-ECHO END UPDATE!
+ECHO UPDATE COMPLETED!
 ECHO ===========================
 
 timeout /t 3
