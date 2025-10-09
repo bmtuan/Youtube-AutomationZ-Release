a1c2048507cdd8faebe9f9ffc76a5e852991f984
tuanbm <tuanbm183653@gmail.com>
2025-10-09 16:27:55 +0700

    fix

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
+import atexit
+
 from nicegui import ui
 
 from src.route_manager import router
+from web.components.loop import cleanup_tkinter_root
 from web.views import *
 
 router.setup_routes()
+
+# Register cleanup function to be called on application shutdown
+atexit.register(cleanup_tkinter_root)
 
 ui.run(
     title="Automation Z",
     favicon="https://cdn-icons-png.flaticon.com/512/8188/8188078.png",
     port=8081,
+)
 )
